68beb89e9a2a9456f2885634c62763e65cde7b52
Sjoerd Simons <sjoerd@collabora.com>
2024-01-30 22:58:05 +0100

    DEBUG protocol hacks

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,40 @@ use attiny_hal::port::{Pin, PB3, PB4};
 use attiny_hal::simple_pwm::{Prescaler, Timer1Pwm};
 use avr_device::attiny85::tc0::tccr0b::CS0_A;
 use avr_device::interrupt::Mutex;
+use avr_hal_generic::hal::blocking::delay::DelayUs;
+use avr_hal_generic::hal::digital::v2::{InputPin, ToggleableOutputPin};
 // old embedded hal
-use avr_hal_generic::hal::digital::v2::InputPin;
 use avr_hal_generic::port::mode::{Floating, Input, Output, PwmOutput};
 use avr_hal_generic::simple_pwm::IntoPwmPin;
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::ErrorType;
+use embedded_hal::digital::{ErrorType, StatefulOutputPin};
 use embedded_hal::pwm::SetDutyCycle;
 use infrared::protocol::nec::NecCommand;
-use infrared::protocol::{Nec, NecDebug};
+use infrared::protocol::Nec;
 use infrared::Receiver;
 use panic_halt as _;
 use tinyrand::RandRange;
 
 type IrPin = FlipPin<Pin<Input<Floating>, PB3>>;
-type IrProto = NecDebug;
+type IrProto = Nec;
 
 //static mut PWM: Option<Pin<PwmOutput<Timer1Pwm>, PB4>> = None;
 static mut LED: Option<Pin<Output, PB4>> = None;
 static mut IR: Option<Receiver<IrProto, IrPin>> = None;
 static mut CLOCK: Option<Clock> = None;
-static mut TIME: u32 = 0;
-static LAST: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+
+#[derive(Default, Clone, Copy)]
+struct Data {
+    offset: u8,
+    cmd: u8,
+    stamps: [u32; 32],
+}
+
+static mut DATA: Mutex<Data> = Mutex::new(Data {
+    offset: 0,
+    cmd: 0,
+    stamps: [0; 32],
+});
 
 #[avr_device::interrupt(attiny85)]
 fn PCINT0() {
@@ -38,50 +50,81 @@ fn PCINT0() {
     let ir = unsafe { IR.as_mut().unwrap() };
     let clock = unsafe { CLOCK.as_ref().unwrap() };
     let now = clock.now();
-    led.toggle();
 
     let pin = ir.pin_mut();
+    let stamp = if pin.is_high().unwrap() {
+        now | 0x8000_0000
+    } else {
+        now
+    };
 
-    if pin.is_high().unwrap() {
-        //led.set_high();
-        let prev = unsafe { TIME };
-        if now > prev {
-            let dt = now - prev;
-            // Less then one second
-            if dt < 1000 {
-                /*
-                avr_device::interrupt::free(|cs| {
-                    let last = LAST.borrow(cs);
-                    if last.get() == 0 {
-                        last.set(dt);
-                    }
-                })
-                */
+    let cmd = match ir.event_instant(now) {
+        Ok(Some(cmd)) => {
+            if cmd.repeat {
+                0x2 // Repeat
+            } else {
+                led.toggle();
+                cmd.cmd
             }
         }
-        unsafe { TIME = now };
-    } else {
-        //led.set_low();
-    }
+        Ok(None) => 0x0,
+        Err(_) => 0x1, // error
+    };
 
-    match ir.event_instant(now) {
-        Ok(Some(cmd)) => avr_device::interrupt::free(|cs| {
-            //led.toggle();
-            if cmd.repeat {
-                let l = LAST.borrow(cs);
-                if l.get() == 0 {
-                    LAST.borrow(cs).set(0x2);
-                }
-            } else {
-                LAST.borrow(cs).set(cmd.bits & 0xff);
+    avr_device::interrupt::free(|cs| {
+        let data = unsafe { DATA.get_mut() };
+        if data.cmd == 0 {
+            data.cmd = cmd;
+            if data.offset < data.stamps.len() as u8 {
+                data.stamps[data.offset as usize] = stamp;
+                data.offset += 1;
             }
-        }),
-        Ok(None) => (),
-        _ => avr_device::interrupt::free(|cs| {
-            //led.toggle();
-            LAST.borrow(cs).set(0x1);
-        }),
+        }
+    });
+}
+
+fn send_u8<L, D>(led: &mut L, mut delay: &mut D, byte: u8)
+where
+    L: StatefulOutputPin,
+    D: DelayUs<u16>,
+{
+    for i in 0..8 {
+        let bit = (byte >> (i)) & 0x1;
+        led.toggle();
+        // high
+        delay.delay_us(530);
+        // low
+        led.toggle();
+        if bit == 0 {
+            delay.delay_us(530)
+        } else {
+            delay.delay_us(1600)
+        }
     }
+}
+
+fn send<L, D>(mut led: L, delay: &mut D, address: u8, command: u8)
+where
+    L: StatefulOutputPin,
+    D: DelayUs<u16>,
+{
+    // header high
+    led.toggle();
+    delay.delay_us(9000);
+
+    // header low
+    led.toggle();
+    delay.delay_us(4500);
+
+    send_u8(&mut led, delay, address);
+    send_u8(&mut led, delay, !address);
+    send_u8(&mut led, delay, command);
+    send_u8(&mut led, delay, !command);
+    // final burst
+    led.toggle();
+    delay.delay_us(560);
+    led.toggle();
+    delay.delay_us(1000);
 }
 
 #[attiny_hal::entry]
@@ -102,6 +145,7 @@ fn main() -> ! {
     let mut led0 = pins.pb0.into_output();
     let mut led1 = pins.pb1.into_output();
     let led4 = pins.pb4.into_output();
+    led0.toggle();
 
     //let mut pwm = pins.pb4.into_output().into_pwm(&mut timer);
     //pwm.enable();
@@ -128,33 +172,23 @@ fn main() -> ! {
 
     let mut delay = attiny_hal::delay::Delay::<attiny_hal::clock::MHz1>::new();
     loop {
-        let last = avr_device::interrupt::free(|cs| LAST.borrow(cs).get());
-        if last == 0x0 {
-            led0.set_high();
-            led1.set_low();
-            for _ in 0..5 {
-                delay.delay_ms(200);
-                led0.toggle();
-                //led1.toggle();
-            }
-        } else if last == 0x1 || last == 0x2 {
-            led0.set_high();
-            led1.set_low();
-            for i in 0..20 {
-                delay.delay_ms(200);
-                led0.toggle();
-                if i % last == 0 {
-                    led1.toggle();
-                }
-            }
-        } else {
-            led0.set_low();
-            for i in 0..100 {
-                led1.toggle();
-                delay.delay_ms(last);
-            }
+        led1.toggle();
+        let data = avr_device::interrupt::free(|cs| *unsafe { DATA.borrow(cs) });
+        led1.toggle();
+        // START sequence
+        send(&mut led0, &mut delay, 0xff, 0xee);
+        send(&mut led0, &mut delay, 0xee, 0xff);
+        // Send command byte if any
+        send(&mut led0, &mut delay, 0xcc, data.cmd);
+        send(&mut led0, &mut delay, 0xcc, data.cmd);
+        for i in 0..data.offset {
+            let byte = data.stamps[i as usize].to_be_bytes();
+            send(&mut led0, &mut delay, byte[0], byte[1]);
+            send(&mut led0, &mut delay, byte[2], byte[3]);
         }
-        delay.delay_ms(1000);
+        // le end
+        send(&mut led0, &mut delay, 0xee, 0xff);
+        send(&mut led0, &mut delay, 0xff, 0xee);
     }
 }
 
@@ -205,9 +239,8 @@ where
     }
 }
 
-/// Based on uno-infra example from avr-hal
 #[avr_device::interrupt(attiny85)]
-fn TIMER0_COMPA() {
+fn TIMER0_OVF() {
     unsafe { CLOCK.as_ref().unwrap().tick() };
 }
 
@@ -220,7 +253,6 @@ impl Clock {
     // Assuming CLK IO is at 1mhz
     const FREQ: u32 = 125_000;
     const PRESCALER: CS0_A = CS0_A::PRESCALE_8;
-    const TOP: u8 = 255;
 
     pub const fn new(tc0: attiny_hal::pac::TC0) -> Clock {
         Clock {
@@ -232,32 +264,36 @@ impl Clock {
     pub fn start(&self) {
         avr_device::interrupt::free(|cs| {
             let tc0 = self.tc0.borrow(cs);
-            // Configure the timer for the above interval (in CTC mode)
-            // Clear Timer on Compare match; top in 0cr0a
-            tc0.tccr0a.write(|w| w.wgm0().ctc());
-            tc0.ocr0a.write(|w| w.bits(Self::TOP));
             tc0.tccr0b.write(|w| w.cs0().variant(Self::PRESCALER));
 
-            // Enable interrupt
-            tc0.timsk.write(|w| w.ocie0a().set_bit());
+            // Enable interrupt on overflow
+            tc0.timsk.write(|w| w.toie0().set_bit());
         })
     }
 
     pub fn now(&self) -> u32 {
         avr_device::interrupt::free(|cs| {
-            let c = self.cntr.borrow(cs);
             let tc0 = self.tc0.borrow(cs);
+            let cnt = tc0.tcnt0.read().bits() as u32;
+            let overflow = tc0.tifr.read().tov0().bit_is_set();
 
-            c.get() + (tc0.tcnt0.read().bits() as u32)
+            let ticks = self.cntr.borrow(cs).get();
+            // If there was an overflow but the counter is
+            // already close to the limit assume the overflow
+            // happened just after reading the cnt bits
+            if overflow && cnt < 200 {
+                ticks.wrapping_add(256 + cnt)
+            } else {
+                ticks.wrapping_add(cnt)
+            }
         })
     }
 
     pub fn tick(&self) {
         avr_device::interrupt::free(|cs| {
             let c = self.cntr.borrow(cs);
-
             let v = c.get();
-            c.set(v.wrapping_add(258));
+            c.set(v.wrapping_add(256));
         });
     }
 }
