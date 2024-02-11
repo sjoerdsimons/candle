b0c4d03bbe88a6841080ad8d2f2c55ecdb7eb6d2
Sjoerd Simons <sjoerd@collabora.com>
2024-02-11 12:23:57 +0100

    Document enabling brown-out detector

--- flash.sh
+++ flash.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+#
+# read fuses: avrdude -c ft232h -p t85   -U hfuse:r:-:h -U lfuse:r:-:h -U efuse:r:-:h
+#
+# default hi: 0xdf -> 0xdd => brown-outlevel = 2.7v
+# write fuses: avrdude -c ft232h -p t85   -U hfuse:w:0xdd:m
 
 avrdude -c ft232h -p t85 -U flash:w:./target/avr-attiny85/release/candle.elf:e
