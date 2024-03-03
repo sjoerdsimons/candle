b6d1e2594af5f95c198845a9b3040d88ad8dc216
Sjoerd Simons <sjoerd@collabora.com>
2024-03-03 16:47:46 +0100

    Make cargo run work via flash script

--- flash.sh
+++ flash.sh
@@ -5,4 +5,4 @@
 # default hi: 0xdf -> 0xdd => brown-outlevel = 2.7v
 # write fuses: avrdude -c ft232h -p t85   -U hfuse:w:0xdd:m
 
-avrdude -c ft232h -p t85 -U flash:w:./target/avr-attiny85/release/candle.elf:e
+avrdude -c ft232h -p t85 -U flash:w:$1:e
